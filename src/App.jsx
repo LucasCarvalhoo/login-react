c747a6a4761ead2a5ac0a687ebbab7b693d3e1ca
LucasCarvalhoo <lucas.carvalho7933@gmail.com>
2025-05-14 21:11:55 -0300

    chore: remoção e ajuste de imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,7 @@
-import { useState } from 'react'
-import { LoginContainer } from "./components/LoginContainer/LoginContainer";
-import { LoginHeader } from "./components/LoginHeader/LoginHeader";
-import { LoginForm } from "./components/LoginForm/LoginForm";
-import './App.css'
+import { LoginPage } from "./components/LoginPage/LoginPage"
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-950 p-4">
-      <LoginContainer>
-        <LoginHeader />
-        <LoginForm />
-      </LoginContainer>
-    </div>
-  )
+  return <LoginPage />
 }
 
 export default App
